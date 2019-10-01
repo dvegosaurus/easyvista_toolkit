04a9f1f297d84067cc2826b7632539776bc384a2
dvegosaurus <romain.gourtay@gmail.com>
2019-10-02 00:00:18 +0200

    cleaned the reference block file

--- src/code_blocks_example.ps1
+++ src/code_blocks_example.ps1
@@ -1,17 +1,9 @@
 #$username = "xxxxxx"         # replace with actual login or encrypt outside the script
 #$password = "xxxxxxxxxxxxxxxxx"  # replace with actual password or encrypt outside the script
 
-# convert login:password to an encrypted string
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
-
-# use the encoded string in the header's request
-$headers = @{
-    ContentType = "application/json"
-    Authorization= ("Basic $base64AuthInfo")
-}
 
 
-$uri = "https://prolival.easyvista.com/api/v1/50005/employees"
+$uri = "https://my.easyvista.com/api/v1/50005/employees"
 Invoke-RestMethod -uri $uri -Method GET -Headers $headers
 
 # create user
@@ -30,24 +22,11 @@ $body =@"
 }
 "@
 
-$uri = "https://prolival.easyvista.com/api/v1/50005/employees"
-Invoke-RestMethod -Headers $headers -uri $uri -Method POST -Body $body -ContentType "application/json"
-
-
-# list user
-$uri = "https://prolival.easyvista.com/api/v1/50005/employees/14659"
-Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
-
 # list tickets
-$uri = "https://prolival.easyvista.com/api/v1/50005/requests"
+$uri = "https://my.easyvista.com/api/v1/50005/requests"
 $data = Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
      # get catalog_request by customer (to get catalog_id)
-$data.records.catalog_request | where {$_.CATALOG_REQUEST_PATH -match "PROL"}
-
-# list les catégories de ticket
-$uri = "https://prolival.easyvista.com/api/v1/50005/catalog-requests"
-$data = Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
-$data.records | ft
+$data.records.catalog_request | where {$_.CATALOG_REQUEST_PATH -match "something"}
 
 # create ticket
  $body = @"
@@ -84,57 +63,20 @@ $data.records | ft
 }
 "@
 
-$uri = "https://prolival.easyvista.com/api/v1/50005/requests"
-Invoke-RestMethod -Headers $headers -uri $uri -Method POST -Body $body -ContentType "application/json"
-
-# list les catégories de ticket
-$uri = "https://prolival.easyvista.com/api/v1/50005/catalog-requests/27001"
-Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
-
-
 # show a specific ticket
-$uri = "https://prolival.easyvista.com/api/v1/50005/requests/i190925_004"
+$uri = "https://my.easyvista.com/api/v1/50005/requests/i190925_004"
 Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
 
 
 # to find a incident Catalog_guid :
 # 1. Find an incident matching your neeed
-# 2. Show the incident "https://prolival.easyvista.com/api/v1/50005/requests/i190925_001"
+# 2. Show the incident "https://my.easyvista.com/api/v1/50005/requests/i190925_001"
 # 3. look at the SD_CATALOG_ID
-# 4. use the SD_CATALOG_ID to grab the catalog guid : "https://prolival.easyvista.com/api/v1/50005/catalog-requests/5086"
+# 4. use the SD_CATALOG_ID to grab the catalog guid : "https://my.easyvista.com/api/v1/50005/catalog-requests/5086"
 
 
 # show ticket description :
 # note that the fucking request is on COMMENT!!!!!!
 
-$uri = "https://prolival.easyvista.com/api/v1/50005/requests/i190925_004/comment"
+$uri = "https://my.easyvista.com/api/v1/50005/requests/i190925_004/comment"
 Invoke-RestMethod -Headers $headers -uri $uri -Method GET 
-
-
-# list all the incident type
-$uri = "https://prolival.easyvista.com/api/v1/50005/catalog-requests"
-Invoke-RestMethod -Headers $headers -uri $uri -Method GET |
-select -ExpandProperty records | 
-select CATALOG_REQUEST_PATH,SD_CATALOG_ID
-
-# how to add autocompletion with a dynamic request
-function test
-{
-    param(
-        [ArgumentCompleter({
-        param ($commandName,$parameterName,$wordToComplete,$commandAst,$fakeBoundParameters)
-        $username = "xxxxxx"
-        $password = "xxxxxxx"
-        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
-        $headers = @{
-            ContentType = "application/json"
-            Authorization= ("Basic $base64AuthInfo")
-        }
-        $uri = "https://prolival.easyvista.com/api/v1/50005/catalog-requests"
-        Invoke-RestMethod -Headers $headers -uri $uri -Method GET |
-        select -ExpandProperty records | 
-        select -ExpandProperty CATALOG_REQUEST_PATH | where {$_ -match $wordToComplete}
-    })]
-    [string]$request
-    )
-}
